633a2884c6bb698506f746ff096557c663236f92
iSatVeerSingh <developer.satveer@gmail.com>
2023-03-15 21:50:52 +0530

    default middlewares added

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,20 @@
-const express = require("express");
-const ViteExpress = require("vite-express");
+import express from "express";
+import ViteExpress from "vite-express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+const { PORT } = process.env || 8000;
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+ViteExpress.listen(app, PORT, () =>
+  console.log("Server is listening on port " + PORT)
 );
