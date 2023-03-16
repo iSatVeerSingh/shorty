007ab43ae5f6a840240d7164fef6d3ccb896212f
iSatVeerSingh <developer.satveer@gmail.com>
2023-03-16 12:18:00 +0530

    tailwind css added

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<h1>This is working</h1>);
+root.render(<App />);
